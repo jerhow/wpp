db6946eec5faf1dd07f25ee05afe9f2ba677600e
Jerry Howard <jerryhoward@fastmail.fm>
2019-12-12 21:20:29 +0000

    Directory + base file + plugin headers for a list builder plugin

---
+++ wp-content/plugins/ghostly-list-builder/ghostly-list-builder.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Ghostly List Builder
+Plugin URI: https://github.com/jerhow/wpp
+Description: An email list building plugin for WordPress.
+Version: 0.1
+Author: Jerry Howard
+Author URI: https://github.com/jerhow
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text-Domain: ghostly-list-builder
+*/
+?>
